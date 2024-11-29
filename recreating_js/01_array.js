7a809b62ee38fe10ad133b5ab4cd23033194cd0a
matt-fonto <mateus.fonto@gmail.com>
2024-11-29 21:05:36 +0100

    refactor: custom array

--- recreating_js/01_array.js
+++ recreating_js/01_array.js
@@ -1,87 +1,111 @@
-class CustomArrayAsArray {
+class CustomArray {
   constructor(...items) {
     this.items = [...items];
   }
 
-  // add element to end
-  push(value) {
-    this.items[this.items.length] = value;
-    return this.items.length;
-  }
-
-  // remove and return last element
   pop() {
+    // check if it's undefined
     if (this.items.length === 0) {
       return undefined;
     }
 
+    // save last item
     const lastItem = this.items[this.items.length - 1];
+
+    // remove last item / reduce the length
     this.items.length -= 1;
+
+    // return last item
     return lastItem;
   }
 
-  // remove and return first element
+  push(...items) {
+    // add one or many elements
+    this.items = [...this.items, ...items];
+    // return new length
+    return this.items.length;
+  }
+
+  // removes first item and return it
   shift() {
     if (this.items.length === 0) {
       return undefined;
     }
 
-    const firstElement = this.items[0];
+    // save first item
+    const firstItem = this.items[0];
+
+    // remove first item
     this.items = this.items.slice(1);
-    return firstElement;
+
+    // return first item
+    return firstItem;
   }
 
-  isEmpty() {
-    return this.size() === 0;
-  }
+  // add one or more items to the beginning
+  unshift(...items) {
+    // add items to beginning
+    this.items = [...items, ...this.items];
 
-  size() {
+    // return new length
     return this.items.length;
   }
 
-  // add elements to the beginning
-  unshift(...values) {
-    this.items = [...values, ...this.items];
-    return this.items.length;
-  }
-
-  // execute callback for each element
+  // apply callback to each item
   forEach(callback) {
+    // loop through each element
     for (let i = 0; i < this.items.length; i++) {
-      // cb(currentElement, i, reference to instance)
+      // apply callback to each element
+      // callback(item, iteration, array)
       callback(this.items[i], i, this);
     }
   }
 
-  // apply a callback to each element and return a new custom array
+  // returns a new array with the result of callback
   map(callback) {
-    const result = new CustomArrayAsArray();
+    // initialize new array
+    const newArr = new CustomArray();
+
+    // loop through each element and apply callback
+    for (let i = 0; i < this.items.length; i++) {
+      newArr.push(callback(this.items[i], i, this));
+    }
+
+    return newArr;
+    // return new array with result from callback
+  }
+
+  // returns a new array with items that match function
+  filter(callback) {
+    const newArr = new CustomArray();
 
     for (let i = 0; i < this.items.length; i++) {
-      result.push(callback(this.items[i], i, this));
+      if (callback(this.items[i], i, this)) {
+        newArr.push(this.items[i]);
+      }
     }
+
+    return newArr;
   }
 
-  toString() {
-    return this.items.join(",");
-  }
+  // some() {}
 
-  get(index) {
-    return this.items[index];
-  }
+  // includes() {}
 
-  // Support for `for ... of` and `Array.from()`
-  [Symbol.iterator]() {
-    let index = 0;
-
-    return {
-      next: () => {
-        if (index < this.items.length) {
-          return { value: this.items[index++], done: false };
-        }
-
-        return { done: true };
-      },
-    };
-  }
+  // concat() {}
 }
+
+const customArr = new CustomArray(1, 2, 3);
+
+console.log(customArr);
+console.log(customArr.pop());
+console.log(customArr.push("hello", 4, 5, 6));
+console.log(customArr);
+
+customArr.forEach((element) => {
+  console.log(element);
+});
+
+const newArr = customArr.map((element) => element * 2);
+
+console.log("newArr", newArr);
---
+++ recreating_js/notes.md
@@ -0,0 +1,20 @@
+## Main Array Methods
+
+### Adding and removing elements
+
+#### Adding
+
+- push(element): Adds one or more elements to the end of the array
+- unshift(element): Adds one or more elements to the beginning
+- concat(array2, ...): Combines two or more arrays
+
+#### Removing
+
+- pop(): Removes and returns last item
+- shift(): Removes and returns the first element
+
+### Iteration and transformation
+
+- forEach(callback): Executes function for each element
+- map(callback): Returns a new array with the results of applying the function to each element
+- filter(callback): Returns a new array containing elements that match the condition
